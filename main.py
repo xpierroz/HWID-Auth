c3c551752892e64a2b5e7d2f51adb11010cf70d4
$xTM <73942099+ICExFS@users.noreply.github.com>
2021-02-17 15:22:33 +0300

    🌴 Main APP

---
+++ main.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+os.system('cls')
+
+import requests
+
+import colorama
+from colorama import init, Fore
+init(convert=True)
+
+import time
+
+def program():
+    print("Hello Word!")
+
+def connect():
+    hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
+
+    rq = requests.get('https://www.google.com/')
+
+    if rq.status_code == 404:
+        print(f'{Fore.LIGHTRED_EX}The URL provided is invalid')
+        return
+    if hwid in rq.text:
+        program()
+    else:
+        print(f'{Fore.LIGHTRED_EX}Your HWID isn\'t in the database')
+        print(f'{Fore.LIGHTRED_EX}Your HWID: {Fore.LIGHTYELLOW_EX}{hwid}')
+        
+connect()
+input(Fore.BLACK)    
+    
